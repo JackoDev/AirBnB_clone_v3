2b717b8dee2ea3b7fb7a896bd4c74933051aa986
dilanrinconhbtn <937@holbertonschool.com>
2020-01-30 22:02:57 -0500

    update 2 if

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -13,4 +13,10 @@ def create_review(place_id):
     """
     Create and returns a new review with the status code 201
     """
-    
+    if not request.json:
+        abort(400, "Not a JSON")
+    if 'user_id' not in request.json:
+        abort(400, "Missing user_id")
+    if 'text' not in request.json:
+        abort(400, "Missing text")
+        
