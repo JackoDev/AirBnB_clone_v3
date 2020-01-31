dafbc11d93de00605aee318295049cc154fa4c08
dilanrinconhbtn <937@holbertonschool.com>
2020-01-30 22:01:54 -0500

    update 1

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,3 +8,9 @@ from models import storage
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 
+@app_views.route('places/<place_id>/reviews', methods=['POST'])
+def create_review(place_id):
+    """
+    Create and returns a new review with the status code 201
+    """
+    
