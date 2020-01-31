88e7635878cbfb6f7034f99071f05c0e232fd8c3
dilanrinconhbtn <937@holbertonschool.com>
2020-01-30 22:14:09 -0500

    finishing 12 task ad authors in readme and authors file

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,6 +8,7 @@ from models import storage
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 
+
 @app_views.route('places/<place_id>/reviews', methods=['POST'])
 def create_review(place_id):
     """
@@ -28,6 +29,7 @@ def create_review(place_id):
         return jsonify(review.to_dict()), 201
     abort(404)
 
+
 @app_views.route('reviews/<review_id>')
 def get_review(review_id):
     """
@@ -37,4 +39,30 @@ def get_review(review_id):
     if review:
         return jsonify(review.to_dict())
     abort(404)
-    
+
+
+@app_views.route('places/<place_id>/reviews')
+def get_reviews(place_id):
+    """
+    List of all Review objects of a Place
+    """
+    reviews = []
+    place = storage.get("Place", place_id)
+    if place:
+        for review in place.reviews:
+            reviews.append(review.to_dict())
+        return jsonify(reviews)
+    abort(404)
+
+
+@app_views.route("/reviews/<review_id>", methods=['DELETE'])
+def delete_review(review_id):
+    """
+    Delete review
+    """
+    review = storage.get("Review", id=review_id)
+    if review:
+        storage.delete(review)
+        storage.save()
+        return jsonify({}), 200
+    abort(404)
